bd97cb591f7e6ba451e6cb18012c3e598ea5daf6
Kim, Sunghwan-Desktop <kgmyh73@gmail.com>
2025-01-11 17:24:10 +0900

    up

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p@r2e9rs$fmm=%#x*8!tk@u##!@n)g9wfid7m1u8@xh2kn@xru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] # 서비스 호스트 설정. *: 모든 호스트
 
 
 # Application definition
